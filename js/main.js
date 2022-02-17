78be19a66d3e1ad8f8b6626387d59810d6a8d192
RedVision228 <raynskyibog@gmail.com>
2022-02-18 01:58:40 +0200

    `

---
+++ js/main.js
@@ -0,0 +1,21 @@
+$(function(){
+    $('.design__slider').slick({
+        dots: false,
+        slidesToShow: 1,
+        variableWidth: true,
+        prevArrow: ' <img class="arrow arrow-left" src="/images/arrow-left.svg" alt=""></img> ' , 
+        nextArrow: ' <img class="arrow arrow-right" src="/images/arrow-right.svg" alt=""></img> ', 
+        responsive:[{
+            breakpoint: 361,
+            settings:{
+                variableWidth: false,
+                slidesToShow: 1,
+            }
+        }]
+        
+    });
+});
+
+
+     
+
