7939449123954946bcc25e23f467365dc3f1f83c
RedVision228 <raynskyibog@gmail.com>
2022-02-18 02:16:10 +0200

    main

--- js/main.js
+++ js/main.js
@@ -3,8 +3,8 @@ $(function(){
         dots: false,
         slidesToShow: 1,
         variableWidth: true,
-        prevArrow: ' <img class="arrow arrow-left" src="/images/arrow-left.svg" alt=""></img> ' , 
-        nextArrow: ' <img class="arrow arrow-right" src="/images/arrow-right.svg" alt=""></img> ', 
+        prevArrow: ' <img class="arrow arrow-left" src="../images/arrow-left.svg" alt=""></img> ' , 
+        nextArrow: ' <img class="arrow arrow-right" src="../images/arrow-right.svg" alt=""></img> ', 
         responsive:[{
             breakpoint: 361,
             settings:{
